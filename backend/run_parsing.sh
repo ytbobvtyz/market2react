db433189a8d28fbdc3a1392f9030b138a414154d
ytbob <ytbob@yandex.ru>
2025-09-19 12:01:49 +0300

    fix run parsing

--- backend/run_parsing.sh
+++ backend/run_parsing.sh
@@ -6,8 +6,14 @@ cd /var/www/market2react/market2react/
 # Активируем виртуальное окружение
 source venv/bin/activate
 
+# Переходим в директорию проекта
+cd /var/www/market2react/market2react/backend
+
 # Запускаем парсинг
 python -m app.utils.parse_on_schedule
+
+# Переходим в директорию проекта
+cd /var/www/market2react/market2react/
 
 # Деактивируем виртуальное окружение
 deactivate
